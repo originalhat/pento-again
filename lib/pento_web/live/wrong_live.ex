3f4cccca02919f505d9b8d4ffb6e25b7c100bf66
Devin Brown <devin.g.brown@gmail.com>
2022-08-28 09:44:08 -0600

    Includes time stamp in guess

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,7 +2,7 @@ defmodule PentoWeb.WrongLive do
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
   end
 
   def handle_event("guess", %{"number" => guess} = data, socket) do
@@ -11,8 +11,12 @@ defmodule PentoWeb.WrongLive do
 
     {
       :noreply,
-      assign(socket, message: message, score: score)
+      assign(socket, message: message, score: score, time: time())
     }
+  end
+
+  def time() do
+    DateTime.utc_now |> to_string
   end
 
   @spec render(any) :: Phoenix.LiveView.Rendered.t()
@@ -21,6 +25,7 @@ defmodule PentoWeb.WrongLive do
     <h1>Your score: <%= @score %></h1>
     <h2>
       <%= @message %>
+      It's <%= @time %>
     </h2>
     <h2>
       <%= for n <- 1..10 do %>
